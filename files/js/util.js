5a8cd7db4e4bbb5a83125f6c56739c2d314df01f
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2018-02-20 20:51:09 +0200

    Fixed only two colours being used in game

--- files/js/util.js
+++ files/js/util.js
@@ -5,7 +5,7 @@ var Utils = {
         }
     },
     settings: {
-        colours: ["red", "blue", /*"yellow", "green", "purple", "pink", "turquoise", "brown"*/]
+        colours: ["red", "blue", "yellow", "green", "purple", "pink", "turquoise", "brown"]
     },
     makeGrid: size => {
         var grid = [];
